4e510279ce25ce01665b328f38229b59327ee36c
metzgegu <guillaume.metzger@wizbii.com>
2021-03-23 00:58:42 +0100

    Migrate to Next.js

---
+++ utils/actions.async.js
@@ -0,0 +1,4 @@
+export const getHskData = async (level) => {
+    const res = await fetch(`https://raw.githubusercontent.com/clem109/hsk-vocabulary/master/hsk-vocab-json/hsk-level-${level}.json`)
+    return await res.json()
+}
