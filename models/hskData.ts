1e98ce93a039a4142549b74b66f3ac59a7dc4d39
metzgegu <guillaume.metzger@wizbii.com>
2021-03-23 21:49:52 +0100

    Add preferences component, typescript configuration and the lint configuration

---
+++ models/hskData.ts
@@ -0,0 +1,7 @@
+export interface HskData {
+  id: number
+  hanzi: string
+  pinyin: string
+  translations: string[]
+  incorrect?: boolean
+}
--- utils/actions.async.js
+++ utils/actions.async.js
@@ -1,4 +1,6 @@
 export const getHskData = async (level) => {
-    const res = await fetch(`https://raw.githubusercontent.com/clem109/hsk-vocabulary/master/hsk-vocab-json/hsk-level-${level}.json`)
-    return await res.json()
+  const res = await fetch(
+    `https://raw.githubusercontent.com/clem109/hsk-vocabulary/master/hsk-vocab-json/hsk-level-${level}.json`
+  )
+  return await res.json()
 }
